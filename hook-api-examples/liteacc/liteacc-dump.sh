7f5a128dbd536c3313d0750e6f261d30cd798839
Richard Holland <richard.holland@starstone.co.nz>
2020-11-18 11:26:25 +0100

    fix example scripts

--- hook-api-examples/liteacc/liteacc-dump.sh
+++ hook-api-examples/liteacc/liteacc-dump.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-../rippled account_objects rGGLq3bp1oMjzFwwXnt3kMVtqgKpcue957 | sed 's/CreateCode".*/CreateCode" : <truncated>/g'
+cd ..
+./rippled account_objects rGGLq3bp1oMjzFwwXnt3kMVtqgKpcue957 | sed 's/CreateCode".*/CreateCode" : <truncated>/g'
 
