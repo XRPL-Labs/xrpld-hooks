08a52bede52286be41e81d52e0658ef34eebe580
Richard Holland <richard.holland@starstone.co.nz>
2021-03-08 17:24:14 +0100

    clean up initial float example

--- hook-api-examples/float_test/float.c
+++ hook-api-examples/float_test/float.c
@@ -12,22 +12,26 @@ int64_t cbak(int64_t reserved) {
 int64_t hook(int64_t reserved ) {
 
 
+    // create a float which is the smallest number representable by rippled
     int64_t y = float_set(-81, 1);
+    
+    // print it
+    trace_float(y);
 
+    //multiply it by 2
+    y = float_mulratio(y, 1 /* round up */, 2 /* numerator */, 1 /* denominator */);
+
+    // print it again
+    trace_float(y);
+
+
+    // serialize the float to an sfAmount STO and print it to log
     uint8_t float_out[9];
     CLEARBUF(float_out);
     int64_t result = float_sto(SBUF(float_out), y, sfAmount);
     if (result < 0)
         rollback(SBUF("float_sto failed"), 1);
     trace(SBUF(float_out), 1);
-
-    TRACEVAR(y);
-    trace_float(y);
-
-
-    y = float_mulratio(y, 1, 2, 1);
-    trace_float(y);
-
 
     trace(SBUF("Float.c: Called."), 0);
     accept (0,0,0); 
